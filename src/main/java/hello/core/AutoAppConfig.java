f63d9621cee3275d8746113237ac7196af37d07b
nefertirii <nefertiri972@gmail.com>
2021-04-14 23:30:04 +0900

    Add base component scan package

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,10 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+//        basePackages 또는 basePackageClasses 를 지정하지 않으면 @ConponentScan 이 붙은 설정 정보 클래스의 패키지가 시작위치가 됨
+//        basePackages = "hello.core.member",
+//        basePackageClasses = AutoAppConfig.class,
+
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
