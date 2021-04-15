724572a20a8d128536557df8bfc3df1bf06575ab
nefertirii <nefertiri972@gmail.com>
2021-04-16 02:07:28 +0900

    Add autowired test

--- src/test/java/hello/core/CoreApplicationTests.java
+++ src/test/java/hello/core/CoreApplicationTests.java
@@ -1,13 +1,23 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.order.OrderService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CoreApplicationTests {
 
+	@Autowired MemberRepository memberRepository;
+	@Autowired OrderService orderService;
+	@Autowired MemberService memberService;
 	@Test
 	void contextLoads() {
+		System.out.println("memberRepository = " + memberRepository);
+		System.out.println("orderService = " + orderService);
+		System.out.println("memberService = " + memberService);
 	}
 
 }
